6c0cd5aa096ba8d4919c368ee137eaaab5f1dd9d
AlekseyTilinin <alexeytilinin@yandex.ru>
2022-11-04 22:59:40 +0300

    HomeWork_4

---
+++ Navigation/Checker.swift
@@ -0,0 +1,38 @@
+//
+//  Checker.swift
+//  Navigation
+//
+//  Created by Aleksey on 04.11.2022.
+//
+
+import Foundation
+import UIKit
+
+protocol LogInViewControllerDelegate {
+    
+    func check(_ sender: LogInViewController, logIn: String, password: String) -> Bool
+}
+
+class Checker {
+    
+    static let shared = Checker()
+    
+    private init() {
+        logIn = "login"
+        password = "password"
+    }
+    
+    private let logIn: String
+    private let password: String
+    
+    func check(logIn: String, password: String) -> Bool {
+        self.logIn == logIn && self.password == password ? true : false
+    }
+}
+
+struct LogInInspector: LogInViewControllerDelegate {
+    
+    func check(_ sender: LogInViewController, logIn: String, password: String) -> Bool {
+        return Checker.shared.check(logIn: logIn, password: password)
+    }
+}
